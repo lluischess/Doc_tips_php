2b1c0c82f8423f84be06881bfc5f5c3e1250e7a0
Lluis Casamajor <lluis.casamajor@nacex.com>
2022-11-09 09:17:43 +0100

    Work whit loops 2

--- 1-Basic/TipsBucles.php
+++ 1-Basic/TipsBucles.php
@@ -37,7 +37,7 @@ endwhile;
 # el do-while es lo mismo que el while pero antes de la condición ejecutara el codigo
 $a = 0;
 do {
-    echo $a++;
+    echo $a++ . "\n";
 } while ($a <= 10);
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
@@ -55,7 +55,81 @@ for ($i = 0; $i < 5; ++$i) {
     print "$i\n";
 }
 
+# Sintaxis HTML
+
+for ($i = 0; $i < 5; ++$i):
+    echo $i;
+endfor;
+
+# se pueden añadir mas codigo separado por comas
+# si añadimos a la condición $i > 5 mas cosas la condición se va a leer de izquierda a derecha
+for ($i = 0; print $i, $i < 5; print $i, ++$i){ // solo funciona el print el echo no
+# print $i, $i < 5 se imprimira el 5 porque la condición de menor esta en la derecha
+}
+
+# Ejemplo de trabajar con array en for
+
+$array = ['a','b','c','d'];
+for ($i = 0, $lenght = count($array); $i < $lenght; $i++){
+    echo $i;
+    echo $array[$i] . "\n";
+}
+
 #----------------------------------------------------------------------------------------------------------------------------------------------
 # 4) foreach
+# Only for arrays and objects
+$programlanguages = ['php','js','python','java'];
+
+foreach($programlanguages as $key => $lengueges){
+    echo $key ."-" . $lengueges . "\n";
+}
+
+# es curioso que la variable del loop foreach $lenguages no es destruida despues del loop y se queda con el valor de la ultima iteración
+echo $lengueges;
+# asi que esta bien destruir la variable para que luego no moleste con unset
+unset($lenguages);
+echo $lenguages;
+
+# tips para recorrer arrays asociativos con foreach
+
+$user = [
+    'name' => 'locol',
+    'email' => 'locol@gmail.com',
+    'skills' => ['php', 'graphql', 'react'],
+];
+
+foreach($user as $key => $value){
+    //echo $key . ": ". $value . "\n"; // cuando llege a skills dara un error de conversión ya que es un array
+    # Solución 1:
+    //echo $key . ": ". json_encode($value) . "\n";
+    # Solución 2:
+    # Revisar que es un array si usas implode
+    if (is_array($value)){
+        $value = implode(',', $value);
+    }
+    echo $key . ": ". $value . "\n";
+}
+
+#Otra forma mas limpia de imprimir asociativos
+
+$user = [
+    'name' => 'locol',
+    'email' => 'locol@gmail.com',
+    'skills' => ['php', 'graphql', 'react'],
+];
+
+foreach($user as $key => $value){
+
+    echo $key . ": ";
+
+    if (is_array($value)){
+        foreach($value as $skills){
+            echo $skills . " - ";
+        }
+    }else{
+        echo $value;
+    }
+    echo "\n";
+}
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
