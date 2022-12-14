d5ab7766d172cd14bc145dbfd619af4a53fe7cb5
Lluis Casamajor <lluis.casamajor@nacex.com>
2022-12-14 08:31:32 +0100

    Work with composer

--- 2-Intermediate/AutoloadingTest/Autoloading.php
+++ 2-Intermediate/AutoloadingTest/Autoloading.php
@@ -5,19 +5,24 @@
 
 // PHP tiene una function para el autoload
 # El primer parametro es un calleble que es nuestra funcion que se ejecuta en ese momento
+# El tema del autoloading puede estar cubierto por composer tambien
 
 # El autoload por defecto usara la clase instanciada, si no tenemos ninguna no se ejecutara
 spl_autoload_register(function($class){
     // Añadimos el path correcto para requerir la clase
-     $class = __DIR__ . '/../' .lcfirst(str_replace('\\', '/', $class)) . '.php';
-     require $class;
-     var_dump($class);
+     $path = __DIR__ . '/../' .lcfirst(str_replace('\\', '/', $class)) . '.php';
+
+    if(file_exists($path)){
+        require $path;
+        var_dump($path);
+    }
+
  });
  
  // El prarametro prepend se usa para que se ejecute primero este autoload
  spl_autoload_register(function($class){
      var_dump('Autoloader 2');
- }, prepend:true);
+ }/*, prepend:true*/);
  
  use tipsOop\tests\Camion;
  use tipsOop\tests\coche;
--- 2-Intermediate/TipsCodeStylePSR.php
+++ 2-Intermediate/TipsCodeStylePSR.php
@@ -1,11 +1,16 @@
 <?php
 
 # INDEX 
-# 1)
+# 1) PSR PHP Standards Recommendations
 # 2) 
 # 3) 
-# 4) 
 
 
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
+# 1) PSR PHP Standards Recommendations
+
+# Es seguir una nomenclatura a la hora de programar codigo limpio y mantenible
+# En la siguiente web tenemos todo tipo de ejemplos de como aplicar PSR a nuestro codigo
+#https://www.php-fig.org/psr/
+# https://github.com/php-fig/fig-standards/tree/master/accepted
